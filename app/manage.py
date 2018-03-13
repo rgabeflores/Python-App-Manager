8d9c35c2f92fe0d6a7e031296478ef06248714ee
rgabeflores <rgabeflores@gmail.com>
2018-03-12 19:52:09 -0700

    Refactored Structure

--- app/manage.py
+++ app/manage.py
@@ -2,43 +2,38 @@ import sys
 import os
 
 '''
-	Generates a boiler plate for new Python projects.
+    Generates a boiler plate for new Python projects.
 
-	Usage:
-	python manage.py <options> <projectname>
+    Usage:
+    python manage.py <options> <projectname>
 
-	Options:
-	-v 		Create project with a virtualenv (must have virtualenv installed)
-	-w 		Create project with Website Files
-	-d 		Start with debug mode on
+    Options:
+    -v      Create project with a virtualenv (must have virtualenv installed)
+    -w      Create project with static Website Files
+    -d      Start with debug mode on
 '''
 
 
 class UsageException(Exception):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, expression, message, *args, **kwargs):
         super().__init__(args, kwargs)
+        self.expression = expression
+        self.message = '''
+            Usage:
+            python manage.py <options> <projectname>
+
+            Options:
+            -v      Create project with virtualenv (must have virtualenv installed)
+            -w      Create project with Website Files
+            -d      Start with debug mode on
+        '''
 
     def __str__(self):
-        return 'Incorrect Usage.'
+        return 'Incorrect Usage.\n{}'.format(self.message)
 
     def __repr__(self):
-        return 'Incorrect Usage.'
-
-
-def usage(error=None):
-    output = '''
-		Usage:
-		python manage.py <options> <projectname>
-
-		Options:
-		-v 		Create project with virtualenv (must have virtualenv installed)
-		-w 		Create project with Website Files
-		-d 		Start with debug mode on
-	'''
-    if error:
-        print(error)
-    print(output, '\n')
+        return 'Incorrect Usage.\n{}'.format(self.message)
 
 
 def construct(path='.', name='base', *args):
@@ -50,40 +45,52 @@ def construct(path='.', name='base', *args):
     except Exception as e:
         print(e)
     else:
-    	if '-v' in args:
+        # FILE CREATION
+        # Conditions reserved to add/remove/edit file lines
+        # based on command line args
+        if '-v' in args:
             pass
         if '-w' in args:
             pass
         if '-d' in args:
             pass
+
+        try:
+            with open('{}/{}.py'.format(path, name), 'w') as f:
+                f.write(base)
+        except Exception as e:
+            print(e, '\nSomething went wrong while creating the boiter plate.\n')
+
+    sys.exit()
+
+
+def construct_test(path='.', name='test_base', *args):
     try:
         with open('./templates/test_base.py') as f:
-            base = f.read()
+            test_base = f.read()
     except FileNotFoundError as e:
         print('The base file is misplaced or missing.')
     except Exception as e:
         print(e)
     else:
-    	if '-v' in args:
+        # FILE CREATION
+        # Conditions reserved to add/remove/edit file lines
+        # based on command line args
+        if '-v' in args:
             pass
         if '-w' in args:
             pass
         if '-d' in args:
             pass
+
+        try:
+            with open('{}/test_{}.py'.format(path, name), 'w') as f:
+                f.write(base)
+        except Exception as e:
+            print(e, '\nSomething went wrong while creating the boiter plate.\n')
+
     sys.exit()
 
-
-def create(content):
-	try:
-        with open('{}/{}.py'.format(path, name), 'a') as f:
-            f.write(base)
-    except Exception as e:
-    	print(e, '\nSomething went wrong while creating the boiter plate.\n')
-    try:
-        with open('{}/test_{}.py'.format(path, name), 'a') as f:
-            f.write(base)
-    except Exception as e:
-    	print(e, '\nSomething went wrong while creating the boiter plate.\n')
 
 def main(args):
     if args:
