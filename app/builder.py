a5d4643bc0eeb1ab9944d39c2aa8d618f1fb0b80
rgabeflores <rgabeflores@gmail.com>
2018-03-12 19:27:19 -0700

    Added Iterative Methods to ProjectBuilder Class

--- app/builder.py
+++ app/builder.py
@@ -2,11 +2,23 @@
 class ProjectBuilder:
 
     def __init__(self, lines, *args, **kwargs):
-        self._lines = list(lines)
+        self.lines = list(lines)
 
     @property
     def lines():
-        return self._lines
+        return self.lines
+
+    def __len__(self):
+        return len(self.lines)
+
+    def __getitem__(self, i):
+        return self.lines[i]
+
+    def __setitem__(self, i, line):
+        if isinstance(line, str):
+            self.lines[i] = line
+        else:
+            pass
 
     def __str__(self):
         pass
